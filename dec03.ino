7bec58f60549adbae81c268d989ff691ccd13e77
SyedMuddassirRaza <75426572+SyedMuddassirRaza@users.noreply.github.com>
2020-12-03 17:53:33 +0500

    Add files via upload

---
+++ dec03.ino
@@ -0,0 +1,31 @@
+char a;
+void setup()
+{
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    a=Serial.read();
+  }
+
+  if (a=='1')
+  {
+    digitalWrite(13,HIGH);
+    Serial.println(a);
+  }
+  else if(a=='2')
+  {
+    digitalWrite(13,LOW);
+    Serial.println(a);    
+  }
+  else
+  {
+    Serial.println("noooo");
+  }
+  delay(1000);
+}
+
